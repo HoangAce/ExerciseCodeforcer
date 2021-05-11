4b711aed34618495727bf87cd5da2ada4925c10d
Nguyễn Huy Hoàng <hoangnhch2006002@fpt.edu.vn>
2021-05-12 05:56:49 +0700

    Update Excerciser

--- 4A.py
+++ 4A.py
@@ -1,5 +1,5 @@
 w = int ( input ())
-if ( ( 4 <= w <= 100 ) và ( w % 2 == 0 )):         
+if ( ( 4 <= w <= 100 ) and ( w % 2 == 0 )):         
     print ( "Có" )
-khác :
+else :
     print ( "Không" )
