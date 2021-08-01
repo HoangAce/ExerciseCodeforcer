fa20630502e06f66a75c00454dd69d9699a3b171
HoangAce <83871529+HoangAce@users.noreply.github.com>
2021-08-01 23:08:40 +0700

    Update Ex

--- 1230A.py
+++ 1230A.py
@@ -1,6 +1,6 @@
 def main():
     a = list(map(int, input().split()))
-    if (max(a) == sum(a) // 2) or ((max(a) + min(a) == sum(a) // 2)):
+    if (max(a) == sum(a) / 2) or ((max(a) + min(a) == sum(a) / 2)):
         print("YES")
     else:
         print("NO")
